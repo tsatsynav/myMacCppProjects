f8e58c1b13c890dcf36c8522fb627c56b642b891
Andrey Tsatsyn <tsatsynav@yandex.ru>
2025-04-27 22:03:22 +0300

    set setting, ready to study

---
+++ cppStepik/testingStepikTasks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppStepik
+//
+//  Created by Andrey Tsatsyn on 27.04.2025.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
